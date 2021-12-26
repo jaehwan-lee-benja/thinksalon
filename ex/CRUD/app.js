e07b2b64b07029c457cf868f766185e00cc753e6
jaehwan <designerbenja@gmail.com>
2021-12-26 16:18:09 +0900

    firebase test

--- ex/CRUD/app.js
+++ ex/CRUD/app.js
@@ -19,3 +19,23 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const userListUI = document.getElementById("userList");
+usersRef.on("child_added", snap => {
+    let user = snap.val();
+    let $li = document.createElement("li");
+    $li.innerHTML = user.name;
+    $li.setAttribute("child-key", snap.key);
+    $li.addEventListener("click", userClicked) userListUI.append($li);
+});
+
+function userClicked(e) {
+  var userID = e.target.getAttribute("child-key");
+  const userRef = dbRef.child('users/' + userID);
+  const userDetailUI = document.getElementById("userDetail");
+  userDetailUI.innerHTML = ""
+  userRef.on("child_added", snap => {
+      var $p = document.createElement("p");
+      $p.innerHTML = snap.key + " - " + snap.val() userDetailUI.append($p);
+  });
+}
