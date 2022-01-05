1122027bcde1bef02f298c5fde4b610ac5e169d4
joey.51 <joey.51@kakaocorp.com>
2022-01-05 18:51:25 +0900

    sample app

--- thinkpaper/app.js
+++ thinkpaper/app.js
@@ -1,226 +1,69 @@
 
 // !IMPORTANT: REPLACE WITH YOUR OWN CONFIG OBJECT BELOW
 
-
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBmGlVK1P-fTw_RvFaA9tV1pEv8-Rk_-z4",
-    authDomain: "thinksalon-2021.firebaseapp.com",
-    databaseURL: "https://thinksalon-2021-default-rtdb.firebaseio.com",
-    projectId: "thinksalon-2021",
-    storageBucket: "thinksalon-2021.appspot.com",
-    messagingSenderId: "892004428811",
-    appId: "1:892004428811:web:805e7e85048e791af6eb0e",
-    measurementId: "G-YE9WY5Z6ZS"
+	apiKey: "AIzaSyBmGlVK1P-fTw_RvFaA9tV1pEv8-Rk_-z4",
+	authDomain: "thinksalon-2021.firebaseapp.com",
+	databaseURL: "https://thinksalon-2021-default-rtdb.firebaseio.com",
+	projectId: "thinksalon-2021",
+	storageBucket: "thinksalon-2021.appspot.com",
+	messagingSenderId: "892004428811",
+	appId: "1:892004428811:web:805e7e85048e791af6eb0e",
+	measurementId: "G-YE9WY5Z6ZS"
 };
-
-
 
 firebase.initializeApp(config);
 
 // Firebase Database Reference and the child
-const dbRef = firebase.database().ref();
-const usersRef = dbRef.child('users');
+const db = firebase.database()
+const usersRef = db.ref("users")
 
 
-	readUserData(); 
-	
+let data = {}
 
-// --------------------------
-// READ
-// --------------------------
-function readUserData() {
-
-	const userListUI = document.getElementById("user-list");
-
-	usersRef.on("value", snap => {
-
-		userListUI.innerHTML = ""
-
-		snap.forEach(childSnap => {
-
-			let key = childSnap.key;
-            console.log('key=',key);
-			let	value = childSnap.val();
-            console.log('value=',value);
-  			
-			let $li = document.createElement("li");
-
-			// edit icon
-			let editIconUI = document.createElement("span");
-			editIconUI.class = "edit-user";
-			editIconUI.innerHTML = " ✎";
-			editIconUI.setAttribute("userid", key);
-			editIconUI.addEventListener("click", editButtonClicked)
-
-			// delete icon
-			let deleteIconUI = document.createElement("span");
-			deleteIconUI.class = "delete-user";
-			deleteIconUI.innerHTML = " ☓";
-			deleteIconUI.setAttribute("userid", key);
-			deleteIconUI.addEventListener("click", deleteButtonClicked)
-			
-			$li.innerHTML = value.age;
-			$li.append(editIconUI);
-			$li.append(deleteIconUI);
-
-			$li.setAttribute("user-key", key);
-			$li.addEventListener("click", userClicked)
-			userListUI.append($li);
-
- 		});
-
-
-	})
-
-}
-
-
-
-function userClicked(e) {
-
-
-		var userID = e.target.getAttribute("user-key");
-
-		const userRef = dbRef.child('users/' + userID);
-		const userDetailUI = document.getElementById("user-detail");
-
-		userRef.on("value", snap => {
-
-			userDetailUI.innerHTML = ""
-
-			snap.forEach(childSnap => {
-				var $p = document.createElement("p");
-				$p.innerHTML = " 내가 왜 이 길을 가고자 하는가, 본질적으로 느끼고 싶은 의미(가치)는 무엇인가? " + childSnap.key  + " - " +  childSnap.val();
-				userDetailUI.append($p);
-			})
-
-		});
-	
-
-}
-
-
-
-
-
-// --------------------------
-// ADD
-// --------------------------
-
-const addUserBtnUI = document.getElementById("add-user-btn");
-addUserBtnUI.addEventListener("click", addUserBtnClicked)
-
-
-
-function addUserBtnClicked() {
-
-	const usersRef = dbRef.child('users');
-
-	const addUserInputsUI = document.getElementsByClassName("user-input");
-
- 	// this object will hold the new user information
-    let newUser = {};
-
-    // loop through View to get the data for the model 
-    for (let i = 0, len = addUserInputsUI.length; i < len; i++) {
-
-        let key = addUserInputsUI[i].getAttribute('data-key');
-        let value = addUserInputsUI[i].value;
-        newUser[key] = value;
-    }
-
-	usersRef.push(newUser)
-
-    
-   console.log(myPro)
-   
-
-
-}
-
-
-// --------------------------
-// DELETE
-// --------------------------
-function deleteButtonClicked(e) {
-
-		e.stopPropagation();
-
-		var userID = e.target.getAttribute("userid");
-
-		const userRef = dbRef.child('users/' + userID);
-		
-		userRef.remove();
-
-}
-
-
-// --------------------------
-// EDIT
-// --------------------------
-function editButtonClicked(e) {
-	
-	document.getElementById('edit-user-module').style.display = "block";
-
-	//set user id to the hidden input field
-	document.querySelector(".edit-userid").value = e.target.getAttribute("userid");
-
-	const userRef = dbRef.child('users/' + e.target.getAttribute("userid"));
-
-	// set data to the user field
-	const editUserInputsUI = document.querySelectorAll(".edit-user-input");
-
-
+function readData() {
+	const userName = document.getElementById('userName').value
+	const userRef = db.ref("users/" + userName)
 	userRef.on("value", snap => {
-
-		for(var i = 0, len = editUserInputsUI.length; i < len; i++) {
-
-			var key = editUserInputsUI[i].getAttribute("data-key");
-					editUserInputsUI[i].value = snap.val()[key];
-		}
-
+		snap.forEach(childSnap => {
+			let key = childSnap.key;
+			let value = childSnap.val();
+			data[key] = value;
+		});
+		// html에 넣기.
+		console.log(data);
+		document.getElementById('date').value = data.date;
+		document.getElementById('direction').innerHTML = data.direction;
+		document.getElementById('navi').innerHTML = data['navi'];	// 이렇게 해도 됨.
+		document.getElementById('action').innerHTML = data.action;
 	});
+};
 
+// 버튼 클릭 시 데이터를 수정하기. *주의 신규입력 아님.
+function onUpdate() {
+	let updatedData = {}
+	updatedData['date'] = document.getElementById('date').value
+	updatedData['direction'] = document.getElementById('direction').value
+	updatedData['navi'] = document.getElementById('navi').value
+	updatedData['action'] = document.getElementById('action').value
 
+	const userName = document.getElementById('userName').value
+	const userRef = db.ref("users/" + userName)
+	userRef.update(updatedData);
+	alert("saved.");
+	location.reload();
+};
 
+// 신규 입력. test라는 이름으로, data를 그대로 입력.
+function onNew() {
+	let newData = {}
+	newData['date'] = document.getElementById('date').value
+	newData['direction'] = document.getElementById('direction').value
+	newData['navi'] = document.getElementById('navi').value
+	newData['action'] = document.getElementById('action').value
 
-	const saveBtn = document.querySelector("#edit-user-btn");
-	saveBtn.addEventListener("click", saveUserBtnClicked)
+	let userName = document.getElementById('userName').value
+	usersRef.child(userName)
+		.set(newData);
 }
-
-
-function saveUserBtnClicked(e) {
- 
-	const userID = document.querySelector(".edit-userid").value;
-	const userRef = dbRef.child('users/' + userID);
-
-	var editedUserObject = {}
-
-	const editUserInputsUI = document.querySelectorAll(".edit-user-input");
-
-	editUserInputsUI.forEach(function(textField) {
-		let key = textField.getAttribute("data-key");
-		let value = textField.value;
-  		editedUserObject[textField.getAttribute("data-key")] = textField.value
-	});
-
-
-
-	userRef.update(editedUserObject);
-
-	document.getElementById('edit-user-module').style.display = "none";
-
-
-}
-
-
-
-        
-
-
-
-
-
-
-
