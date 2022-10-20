8a61e47a856a45260320f39cefef49f0091a08a6
jaehwan-lee-benja <designerbenja@gmail.com>
2022-10-20 17:33:22 +0900

    테스트 영역 만들기 완료

---
+++ thinkpaper/dept_0_test.js
@@ -0,0 +1,9 @@
+(function() {
+    const testTitle = document.getElementById("testTitle");
+    testTitle.innerHTML = "hello";
+})();
+
+function countRow(layerHere) {
+	const idArray = Object.keys(objectById);
+	console.log("idArray = ", idArray);
+}; // 테스트 js 파일이 하나 필요하다.
--- thinkpaper/dept_li_support.js
+++ thinkpaper/dept_li_support.js
@@ -149,7 +149,3 @@ function getLayerById(idHere) {
 	})
 	return layer;
 };
-function countRow(layerHere) {
-	const idArray = Object.keys(objectById);
-	console.log("idArray = ", idArray);
-}; // 테스트 js 파일이 하나 필요하다.
